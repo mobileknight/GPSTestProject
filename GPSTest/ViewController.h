f50e85c291a79d67b6bbf21fe3dc63bba7910a83
mobileknight <mobileknight19900712@gmail.com>
2016-05-07 04:30:59 +0800

    Initial Project

---
+++ GPSTest/ViewController.h
@@ -0,0 +1,18 @@
+//
+//  ViewController.h
+//  GPSTest
+//
+//  Created by user on 5/7/16.
+//  Copyright © 2016 knight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
+
+@interface ViewController : UIViewController
+{
+    CLLocation *p;
+}
+
+@end
+
